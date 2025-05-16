b3786a0288004ca8d46d864340f1e8375b813b77
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-15 22:42:44 -0400

    updates made to Head

--- head.js
+++ head.js
@@ -22,4 +22,4 @@ const head = function(array){
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Empty");
+assertEqual(head([]), undefined);
