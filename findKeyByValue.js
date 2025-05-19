fdff86d95e06aff2d9d20eee257476dac6cc021e
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-19 13:59:36 -0400

    findValueByKey function for Lotide project

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const findKeyByValue = function (List , Show){
+ for(let items in List) {
+  if(Show == List[items]){
+    return items
+  }
+}
+return undefined
+}
+
+
+
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//Test Code
+
+assertEqual("Lighthouse Labs" , "Bootcamp");
+assertEqual(1,1);
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"),"comedy")
