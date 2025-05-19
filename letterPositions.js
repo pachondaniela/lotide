fd1843eb5dfbecc6db227e3fb81260a2304d847f
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-19 13:30:51 -0400

    letterPositions function inside Lotide project

---
+++ letterPositions.js
@@ -0,0 +1,51 @@
+const letterPositions = function(sentence){
+  const results = {}
+
+  for (let i = 0; i < sentence.length ; i++) {
+      const char = sentence[i]
+
+      if (char === " ") continue;
+
+      if (results.hasOwnProperty(char)){
+        results[char].push(i);
+      } else {
+        results[char] = [i]
+      }
+  }
+
+  return results
+
+}
+
+// // Function Implementation
+
+const eqArrays = function(array1, array2) {
+  if(array1.length !== array2.length){
+      return false;
+  } 
+    for (let i = 0; i < array1.length ; i++){
+      if(array1[i] !== array2[i]){
+        return false
+      } 
+    }
+    return true
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(` ✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else  {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+}
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [3, 2, 1])
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("I love You").o, [3,8]);
