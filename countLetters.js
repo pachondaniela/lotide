74bbf6188937db1283cd7cd962b9ebd99747a56b
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-19 13:13:42 -0400

    updated countLetters function

--- countLetters.js
+++ countLetters.js
@@ -30,18 +30,7 @@ const countLetters = function (string) {
 
 
 
-test = {
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
-}
+
 
 
 assertEqual(countLetters("lighthouse")["l"], 1);
