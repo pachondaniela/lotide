126ebafe5baef8bb3bfaf71e8777312707dfef80
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-20 20:21:49 -0400

    findKey function for the Lotide function

---
+++ findKey.js
@@ -0,0 +1,57 @@
+const findKey = function (object, callback){
+  for (let key in object){
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined
+}
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//Test Code
+
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+), "noma");
+
+
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 3
+), "Akaleri");
+
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 1
+), "Blue Hill");
