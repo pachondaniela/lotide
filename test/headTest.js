355c237f2a2e6f64c2eb82c05408bda8dc84ec91
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-26 14:18:50 -0400

    test with mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,21 @@
-// Import the function writted for head
+const assert = require('chai').assert;
+const head = require('../head');
 
-const head = require('../head.js')
-const assertEqual = require('../assertEqual.js')
+describe("#head" , () => {
+  it("returns 1 for [1,2,3]" , () => {
+    assert.strictEqual(head([1,2,3]), 1);
+    });
+});
 
+ it("returns '5' for [5,6,7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5); 
+  });
 
-//Test Code
+it("returns 'Hello' for [Hello, lighthouse, Labs] " , () => {
+  assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+});
+
+it('returns undefined for an empty array' , () => {
+  assert.strictEqual(head([]),undefined);
+});
