6be163c5aa57fff1977fc7bc0eae52673bb493be
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-26 14:21:01 -0400

    updated headTest

--- test/headTest.js
+++ test/headTest.js
@@ -5,17 +5,30 @@ describe("#head" , () => {
   it("returns 1 for [1,2,3]" , () => {
     assert.strictEqual(head([1,2,3]), 1);
     });
-});
 
- it("returns '5' for [5,6,7]", () => {
+  it("returns '5' for [5,6,7]", () => {
     assert.strictEqual(head([5, 6, 7]), 5); 
+  }); 
+
+  it("returns 'Hello' for [Hello, lighthouse, Labs] " , () => {
+  assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello");
   });
 
-it("returns 'Hello' for [Hello, lighthouse, Labs] " , () => {
-  assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello");
-
-});
-
-it('returns undefined for an empty array' , () => {
+  it('returns undefined for an empty array' , () => {
   assert.strictEqual(head([]),undefined);
 });
+
+});
+
+//  it("returns '5' for [5,6,7]", () => {
+//     assert.strictEqual(head([5, 6, 7]), 5); 
+//   });
+
+// it("returns 'Hello' for [Hello, lighthouse, Labs] " , () => {
+//   assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello");
+
+// });
+
+// it('returns undefined for an empty array' , () => {
+//   assert.strictEqual(head([]),undefined);
+// });
